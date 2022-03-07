def32cea7e034ae6338d5d79d0513b17923ab44b
DimDone <kitaev-mitynka@mail.ru>
2022-03-07 22:15:21 +0300

    Changed the initial data from 1 and 1 to 0 and 1

--- fib.cpp
+++ fib.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
-
 using namespace std;
 
 int fib(int n){
-	if(n == 1 || n == 2) return 1;
-	return fib(n-1)+fib(n-2);
+        if(n == 1) return 0;
+        if(n == 2) return 1;
+        return fib(n-1)+fib(n-2);
 }
 
-int main(){
-	cout << fib(10) << endl;
+int main() {
+        cout<< "Hello World!"<< endl;
+        cout << fib(10)<<endl;
 }
